b12fd00ec9178736faa5bc8d153bf22b5481b2f8
Tessen Thế <nvthe@fresher.misa.com.vn>
2020-10-12 16:47:14 +0700

    push

---
+++ Constants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DapperEFCorePerformanceBenchmarks
+{
+    public static class Constants
+    {
+        /// <summary>
+        /// The connection string is not included in the sample project as, in my setup, it was too unique.
+        /// Consequently you will need to set up your own connection to a SQL database and use your own connection string.
+        /// </summary>
+        //public static readonly string SportsConnectionString = "server=10.0.6.14;port=3306;user=root;password=12345678@Abc;database=dapper_efcore";
+        public static readonly string SportsConnectionString = "Server=localhost\\MSSQLSERVER01;Database=TEST;Trusted_Connection=True"; 
+    }
+}
---
+++ DataAccess/EntityFrameworkCoreWithTracking.cs
@@ -0,0 +1,92 @@
+﻿using DapperEFCorePerformanceBenchmarks.DataAccess;
+using DapperEFCorePerformanceBenchmarks.Models;
+using DapperEFCorePerformanceBenchmarks.TestData;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using System.Linq;
+
+public class EntityFrameworkCoreWithTracking : ITestSignature
+{
+    public long GetPlayerByID(int id)
+    {
+        Stopwatch watch = new Stopwatch();
+        watch.Start();
+        using (SportContextEfCore context = new SportContextEfCore(Database.GetOptions()))
+        {
+            var player = context.Players.First(x=>x.Id == id);
+        }
+        watch.Stop();
+        return watch.ElapsedMilliseconds;
+    }
+
+public long GetRosterByTeamID(int teamId)
+{
+    Stopwatch watch = new Stopwatch();
+    watch.Start();
+    using (SportContextEfCore context = new SportContextEfCore(Database.GetOptions()))
+    {
+        var teamRoster = context.Teams.Include(x => x.Players).Single(x => x.Id == teamId);
+    }
+    watch.Stop();
+    return watch.ElapsedMilliseconds;
+}
+
+public long GetTeamRostersForSport(int sportId)
+{
+    Stopwatch watch = new Stopwatch();
+    watch.Start();
+    using (SportContextEfCore context = new SportContextEfCore(Database.GetOptions()))
+    {
+        var players = context.Teams.Include(x => x.Players).Where(x => x.SportId == sportId).ToList();
+    }
+    watch.Stop();
+    return watch.ElapsedMilliseconds;
+}
+    public long Create()
+    {
+        Stopwatch watch = new Stopwatch();
+        Sport sport = new Sport() {
+            Id = 11,
+            Name = "Esport"
+        };
+        watch.Start();
+        using (SportContextEfCore context = new SportContextEfCore(Database.GetOptions()))
+        {
+            context.Sports.Add(sport);
+            context.SaveChanges();
+        }
+        watch.Stop();
+        return watch.ElapsedMilliseconds;
+    }
+    public long Update()
+    {
+        Stopwatch watch = new Stopwatch();
+        
+        watch.Start();
+        using (SportContextEfCore context = new SportContextEfCore(Database.GetOptions()))
+        {
+            var entity = context.Sports.Where(x => x.Id == 11)
+                .SingleOrDefault();
+            entity.Name = "Điện tử";
+            context.Entry(entity).State = EntityState.Modified;
+            context.SaveChanges();
+
+        }
+        watch.Stop();
+        return watch.ElapsedMilliseconds;
+    }
+    public long Delete()
+    {
+        Stopwatch watch = new Stopwatch();
+        watch.Start();
+        using (SportContextEfCore context = new SportContextEfCore(Database.GetOptions()))
+        {
+            var entity = context.Sports.Where(x => x.Id == 11)
+                .SingleOrDefault();
+            context.Sports.Remove(entity);
+            context.SaveChanges();
+        }
+        watch.Stop();
+        return watch.ElapsedMilliseconds;
+    }
+}
